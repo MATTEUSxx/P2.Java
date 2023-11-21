5b9690a8c5dc65577789efe0bca84ba5c4062717
Mateus Lira <mateteu7@hotmail.com>
2023-11-21 18:01:23 +0000

    .

--- app/src/main/java/application/model/alternativa.java
+++ app/src/main/java/application/model/alternativa.java
@@ -1,5 +1,17 @@
 package application.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.Column;
+import javax.persistence.CascadeType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import javax.persistence.*;
 
 @Entity
